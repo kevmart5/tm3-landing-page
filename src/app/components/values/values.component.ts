01f9ec56b11168ff076ef0bac102639f357da008
Kevin Martinez <mkevin755@gmail.com>
2021-05-22 17:10:48 -0600

    Add landing page files

---
+++ src/app/components/values/values.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-values",
+  templateUrl: "./values.component.html",
+  styleUrls: ["./values.component.scss"],
+})
+export class ValuesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
